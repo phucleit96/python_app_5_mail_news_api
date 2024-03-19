26f9772fb8af879b6ba051f89d264864170ff1e7
Phuc Proton <phuc.le.it96@gmail.com>
2024-03-20 02:47:19 +0700

    Sending mail and collecting news

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import requests
+from send_email import send_email
 import json
 import os
 
 api_key = "532cecc741c2449aa9e590c8fd75c57f"
 # api_key = os.getenv('NEWS_API_KEY')
-url = f"https://newsapi.org/v2/everything?q=tesla&from=2024-02-18&sortBy=publishedAt&apiKey={api_key}"
+
+url = f"https://newsapi.org/v2/everything?q=tesla&from=2024-02-19&sortBy=publishedAt&apiKey={api_key}"
 # Make a request
 request = requests.get(url)
 # Get a dictionary with data
 content = request.json()
 
 # Access the article titles and description:
+body = ""
 for article in content['articles']:
-    print(article['title'])
-    print(article['description'])
+    title = article['title'] if article['title'] is not None else ""
+    description = article['description'] if article['description'] is not None else ""
+    body += title + "\n" + description + 2*"\n"
+
+body = body.encode('utf-8')
+
+send_email(message=body)
---
+++ send_email.py
@@ -0,0 +1,18 @@
+import smtplib
+import os
+import ssl
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "badboy27796@gmail.com"
+    password = os.getenv("PASSWORD")
+    receiver = "phuc.le.it96@gmail.com"
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, msg=message)
+
+
+send_email(message="test")
