afcd7f54a7e7acb44e39c50829b679ee1b6b87f1
Phuc Proton <phuc.le.it96@gmail.com>
2024-03-20 03:11:02 +0700

    adjusting dynamic date in URL

--- main.py
+++ main.py
@@ -2,11 +2,28 @@ import requests
 from send_email import send_email
 import json
 import os
+from datetime import datetime, timedelta
+import calendar
+
+now = datetime.now()
+
+# Subtract one day
+one_day_before = now - timedelta(days=1)
+
+# Calculate the last day of the previous month
+if one_day_before.month == 1:
+    one_month_before = one_day_before.replace(year=one_day_before.year-1, month=12, day=calendar.monthrange(one_day_before.year-1, 12)[1])
+else:
+    one_month_before = one_day_before.replace(month=one_day_before.month-1, day=calendar.monthrange(one_day_before.year, one_day_before.month-1)[1])
+
+formatted_date = one_month_before.strftime("%Y-%m-%d")
+
+
 
 api_key = "532cecc741c2449aa9e590c8fd75c57f"
 # api_key = os.getenv('NEWS_API_KEY')
 
-url = f"https://newsapi.org/v2/everything?q=tesla&from=2024-02-19&sortBy=publishedAt&apiKey={api_key}"
+url = f"https://newsapi.org/v2/everything?q=tesla&from={formatted_date}&sortBy=publishedAt&apiKey={api_key}"
 # Make a request
 request = requests.get(url)
 # Get a dictionary with data
